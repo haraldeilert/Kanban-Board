5a2b353e82d52f0cf1bdce18ccc38f7ff6ebdae5
harald.eilert@gmail.com <strapp20>
2012-02-22 20:00:03 +0100

    jquery added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,9 +9,20 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-    	List noterows = NoteRow.find("order by id desc").fetch();
-        render(noterows);
-    }
+	public static void index() {
+		List<NoteRow> noterows = NoteRow.find("order by id desc").fetch();
+		String cssStr = createCSSNameStr(noterows);
+		render(noterows, cssStr);
+	}
+
+	public static String createCSSNameStr(List<NoteRow> noterows) {
+		String tmp = "";
+		for (NoteRow noteRow : noterows) {
+			tmp += "#sortable" + String.valueOf(noteRow.getId().intValue()) + ",";
+			System.out.println(tmp);
+		}
+		System.out.println("return: " + tmp.substring(0, tmp.length() - 1));
+		return tmp.substring(0, tmp.length() - 1);
+	}
 
 }
