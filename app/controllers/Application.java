f2d72be8dbe3ed91eaaadcaaec07ccce5cd49781
harald.eilert@gmail.com <strapp20>
2012-02-23 13:20:46 +0100

    Bootstrap added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,14 +11,15 @@ public class Application extends Controller {
 
 	public static void index() {
 		List<NoteRow> noterows = NoteRow.find("order by id desc").fetch();
-		String cssStr = createCSSNameStr(noterows);
-		render(noterows, cssStr);
+		String cssStr = createCSSNameStr(noterows, "");
+		String cssStr2 = createCSSNameStr(noterows, " li");
+		render(noterows, cssStr, cssStr2);
 	}
 
-	public static String createCSSNameStr(List<NoteRow> noterows) {
+	public static String createCSSNameStr(List<NoteRow> noterows, String ext) {
 		String tmp = "";
 		for (NoteRow noteRow : noterows) {
-			tmp += "#sortable" + String.valueOf(noteRow.getId().intValue()) + ",";
+			tmp += "#sortable" + String.valueOf(noteRow.getId().intValue()) + ext + ",";
 			System.out.println(tmp);
 		}
 		System.out.println("return: " + tmp.substring(0, tmp.length() - 1));
