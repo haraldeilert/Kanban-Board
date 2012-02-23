94cb81decc1551ac54645bbd7543f91696bdc7b7
harald.eilert@gmail.com <strapp20>
2012-02-23 15:41:20 +0100

    more changes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,13 +16,20 @@ public class Application extends Controller {
 		render(noterows, cssStr, cssStr2);
 	}
 
-	public static String createCSSNameStr(List<NoteRow> noterows, String ext) {
+	
+	public static void addNewNote(String title, String noterowtitle) {
+		
+		NoteRow noteRow = NoteRow.find("byTitle", noterowtitle).first();
+		Note note = Note.find("noteRow.title = ? order by positionInRow desc", noteRow.title).first();
+		
+		noteRow.addNote(title, title, note.positionInRow + 1);
+	}
+	
+	private static String createCSSNameStr(List<NoteRow> noterows, String ext) {
 		String tmp = "";
 		for (NoteRow noteRow : noterows) {
-			tmp += "#sortable" + String.valueOf(noteRow.getId().intValue()) + ext + ",";
-			System.out.println(tmp);
+			tmp += "#sortable" + String.valueOf(noteRow.getId().intValue()) + ext + ",";	
 		}
-		System.out.println("return: " + tmp.substring(0, tmp.length() - 1));
 		return tmp.substring(0, tmp.length() - 1);
 	}
 
--- app/models/NoteRow.java
+++ app/models/NoteRow.java
@@ -31,7 +31,7 @@ public class NoteRow extends Model {
 	    this.save();
 	    return this;
 	}
-	
+
 	public String toString() {
 		return title;
 	}
