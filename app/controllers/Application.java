fb38805aab8cb3debe555612d4129ca1d9a2bd40
harald.eilert@gmail.com <strapp20>
2012-02-24 12:27:18 +0100

    More html stuff

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,15 +10,13 @@ import models.*;
 public class Application extends Controller {
 
 	public static void index() {
-		List<NoteRow> noterows = NoteRow.find("order by id desc").fetch();
+		List<NoteRow> noterows = NoteRow.find("order by position asc").fetch();
 		String cssStr = createCSSNameStr(noterows, "");
 		String cssStr2 = createCSSNameStr(noterows, " li");
 		render(noterows, cssStr, cssStr2);
 	}
 
 	public static void addNewNote(Long id, String title) {
-		System.out.println("******id: " + id);
-		System.out.println("******title: " + title);
 		
 		NoteRow noteRow = NoteRow.findById(id);
 
@@ -37,7 +35,11 @@ public class Application extends Controller {
 		for (NoteRow noteRow : noterows) {
 			tmp += "#sortable" + String.valueOf(noteRow.getId().intValue())
 					+ ext + ",";
+			
 		}
+		System.out.println("******test: " + tmp);
+		
+		
 		return tmp.substring(0, tmp.length() - 1);
 	}
 
